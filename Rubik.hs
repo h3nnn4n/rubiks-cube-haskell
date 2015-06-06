af3e381dffdb230a7880b65f3780cd13b2ef09bc
h3nnn4n <uber.renan@gmail.com>
2015-06-06 18:24:35 +0000

    Defined teh edges and their inverses to determine the orientation

--- Rubik.hs
+++ Rubik.hs
@@ -11,23 +11,38 @@ data Edge = Edge Facet Facet deriving (Show, Eq) -- An edge is made out of 2 fac
 data Corner = Corner Facet Facet Facet deriving (Show, Eq) -- An corner is made out of 3 faces
 data Facet = Red | Orange | Yellow | White | Green | Blue deriving (Show, Eq) -- A facet has one of those six colors
 
+-- -- All the cublets -- --
 -- The Edges in the top layer
-uf = (Edge White Red)
-ur = (Edge White Blue)
-ub = (Edge White Orange)
-ul = (Edge White Green)
+uf = (Edge White Red     )
+ur = (Edge White Blue    )
+ub = (Edge White Orange  )
+ul = (Edge White Green   )
 
 -- The Edges in the middle layer
-fr = (Edge Red Blue)
-fl = (Edge Red Green)
-br = (Edge Orange Blue)
-bl = (Edge Orange Green)
+fr = (Edge Red    Blue   )
+fl = (Edge Red    Green  )
+br = (Edge Orange Blue   )
+bl = (Edge Orange Green  )
 
 -- The edges in the buttom layer
-df = (Edge Yellow Red)
-dr = (Edge Yellow Blue)
-dl = (Edge Yellow Green)
-db = (Edge Yellow Orange)
+df = (Edge Yellow Red    )
+dr = (Edge Yellow Blue   )
+dl = (Edge Yellow Green  )
+db = (Edge Yellow Orange )
+
+-- All of the above but inverted
+fu = (Edge Red    White  )
+ru = (Edge Blue   White  )
+bu = (Edge Orange White  )
+lu = (Edge Green  White  )
+rf = (Edge Blue   Red    )
+lf = (Edge Green  Red    )
+rb = (Edge Blue   Orange )
+lb = (Edge Green  Orange )
+fd = (Edge Red    Yellow )
+rd = (Edge Blue   Yellow )
+ld = (Edge Green  Yellow )
+bd = (Edge Orange Yellow )
 
 -- The Corners in the top layer
 ufr = (Corner White Red Blue)
@@ -40,6 +55,40 @@ dfr = (Corner Yellow Red Blue)
 dfl = (Corner Yellow Red Green)
 dbr = (Corner Yellow Orange Blue)
 dbl = (Corner Yellow Orange Green)
+
+-- A function ta returns the inverse of a edge
+-- It is obviously correct, unless there is some typo :)
+inverted (Edge Red    White  ) = (Edge White  Red    )
+inverted (Edge Blue   White  ) = (Edge White  Blue   )
+inverted (Edge Orange White  ) = (Edge White  Orange )
+inverted (Edge Green  White  ) = (Edge White  Green  )
+inverted (Edge Blue   Red    ) = (Edge Red    Blue   )
+inverted (Edge Green  Red    ) = (Edge Red    Green  )
+inverted (Edge Blue   Orange ) = (Edge Orange Blue   )
+inverted (Edge Green  Orange ) = (Edge Orange Green  )
+inverted (Edge Red    Yellow ) = (Edge Yellow Red    )
+inverted (Edge Blue   Yellow ) = (Edge Yellow Blue   )
+inverted (Edge Green  Yellow ) = (Edge Yellow Green  )
+inverted (Edge Orange Yellow ) = (Edge Yellow Orange )
+
+inverted (Edge White  Red    ) = (Edge Red    White  )
+inverted (Edge White  Blue   ) = (Edge Blue   White  )
+inverted (Edge White  Orange ) = (Edge Orange White  )
+inverted (Edge White  Green  ) = (Edge Green  White  )
+inverted (Edge Red    Blue   ) = (Edge Blue   Red    )
+inverted (Edge Red    Green  ) = (Edge Green  Red    )
+inverted (Edge Orange Blue   ) = (Edge Blue   Orange )
+inverted (Edge Orange Green  ) = (Edge Green  Orange )
+inverted (Edge Yellow Red    ) = (Edge Red    Yellow )
+inverted (Edge Yellow Blue   ) = (Edge Blue   Yellow )
+inverted (Edge Yellow Green  ) = (Edge Green  Yellow )
+inverted (Edge Yellow Orange ) = (Edge Orange Yellow )
+
+-- Clockwise rotation of a corner piece
+-- TODO
+
+-- Counter-Clockwise rotation of a corner piece
+-- TODO
 
 -- The cube in a solved state
 solved = (Cube (Edges uf  ul  ub  ur
