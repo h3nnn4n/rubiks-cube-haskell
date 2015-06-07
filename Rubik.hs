6a61484d1449a9dff279399bd6257235706ff1f2
h3nnn4n <uber.renan@gmail.com>
2015-06-07 18:24:41 +0000

    Fixed orientation in L turns. Added some comments and code divisors + Ja, Jb, T and F perms.

--- Rubik.hs
+++ Rubik.hs
@@ -152,16 +152,24 @@ solved = (Cube (Edges uf  ul  ub  ur
 -- Returns true if the cube is solved
 isSolved x = x == solved
 
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+----------------------------------------------------------------------------------------------------------------------------------------------------------------
+
 --Sample Function for a move
 --move_I (Cube (Edges a b c d e f g h i j k l) FCorners m n o p q r s t)) = (Cube (Edges a b c d e f g h i j k l) (Corners m n o p q r s t))
 --move_U :: Cube -> Cube
-move_U (Cube (Edges a b c d e f g h i j k l) (Corners m n o p q r s t)) = (Cube (Edges d a b c e f g h i j k l) (Corners p m n o q r s t))
+move_U (Cube (Edges a b c d e f g h i j k l)
+           (Corners m n o p q r s t)) = (Cube
+             (Edges d a b c e f g h i j k l)
+           (Corners p m n o q r s t))
 
 -- Since a cube face is a rotation group it is possible to define U2 as U two times and U' as U three times
 -- Looks nice and it is obviously both code and mathematically correct, not very speed efficient though
 move_U2 x = move_U $ move_U x
 move_U' x = move_U $ move_U $ move_U x
 
+-------------------------------------------
 --move_R :: Cube -> Cube
 move_R2 x = move_R $ move_R x
 move_R' x = move_R $ move_R $ move_R x
@@ -174,33 +182,62 @@ move_R (Cube (Edges a b c d e f g h i j k l)
                       (turn_ccw t) r s
                       (turn_cw p)))
 
+-------------------------------------------
+
 move_F2 x = move_F $ move_F x
 move_F' x = move_F $ move_F $ move_F x
 
 move_F (Cube (Edges a b c d e f g h i j k l)
-             (Corners m n o p q r s t)) =
-             (Cube (Edges (f) b c d (inverted a) (inverted i) g h (e) j k l)
-             (Corners (turn_ccw n)
+           (Corners m n o p q r s t)) = (Cube
+             (Edges f b c d (inverted a) (inverted i) g h e j k l)
+           (Corners (turn_ccw n)
                       (turn_cw r) o p
                       (turn_ccw m)
                       (turn_cw q) s t))
+
+-------------------------------------------
 
 move_L2 x = move_L $ move_L x
 move_L' x = move_L $ move_L $ move_L x
 
 move_L (Cube (Edges a b c d e f g h i j k l)
            (Corners m n o p q r s t)) = (Cube
-             (Edges a f c d e j b h i g k l)
-           (Corners m (turn_ccw r)
-                      (turn_cw n) p q
-                      (turn_cw s)
-                      (turn_ccw o) t))
+             (Edges a g c d e b j h i f k l)
+           (Corners m (turn_cw o)
+                      (turn_ccw s) p q
+                      (turn_cw n)
+                      (turn_ccw r) t))
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Since the expression is evaluated right to left, we must write the Cube algorithm reversed
 -- R2 U' R' U' R U R U R U' R
 perm_ua x = move_R2 $ move_U' $ move_R' $ move_U' $ move_R $ move_U $ move_R $ move_U $ move_R $ move_U' $ move_R x
+
 -- R' U R' U' R' U' R' U R U R2
 perm_ub x = move_R' $ move_U $ move_R' $ move_U' $ move_R' $ move_U' $ move_R' $ move_U $ move_R $ move_U $ move_R2 x
+
+-- Ja perm
+-- R' U L' U2 R U' R' U2 L R U'
+perm_ja x = move_U' $ move_R $ move_L $ move_U2 $ move_R' $ move_U' $ move_R $ move_U2 $ move_L' $ move_U $ move_R' x
+
+-- Jb perm
+-- R U R' F' R U R' U' R' F R2 U' R' U'
+perm_jb x = move_U' $ move_R' $ move_U' $ move_R2 $ move_F $ move_R' $ move_U' $ move_R' $ move_U $ move_R $ move_F' $ move_R' $ move_U $ move_R x
+
+-- T perm
+-- R U R' U' R' F R2 U' R' U' R U R' F'
+perm_t x = move_F' $ move_R' $ move_U $ move_R $ move_U' $ move_R' $ move_U' $ move_R2 $ move_F $ move_R' $ move_U' $ move_R' $ move_U $ move_R x
+
+-- F perm
+-- R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R
+perm_f x = move_R $ move_U $ move_R' $ move_U $ move_R $ move_U' $ move_R' $ move_U' $ move_R2 $ move_F $ move_R' $ move_U' $ move_R' $ move_U $ move_R $ move_F' $ move_U' $ move_R' x
+
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------------------------------------
 
 -- Dummy cycles for testing
 -- All three should be of size 105 since they are the same cycle, but rotated
@@ -208,9 +245,9 @@ dummy_ru x = move_U $ move_R x
 dummy_fu x = move_U $ move_F x
 dummy_rf x = move_F $ move_R x
 dummy_fr x = move_R $ move_F x
-dummy_lu x = move_U $ move_L' x
-dummy_lf x = move_L' $ move_F x
-dummy_fl x = move_L $ move_F' x
+dummy_lu x = move_U $ move_L x
+dummy_lf x = move_L $ move_F x
+dummy_fl x = move_L $ move_F x
 
 -- Finds the size of a cycle
 cycleSize x = 1 + (length $ takeWhile (\y -> isSolved y == False) $ iterate x (x solved))
